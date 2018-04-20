4a25b7181cdc78779f84c34922c0583f0d9e6887
quhaiming <quhaiming@qiyi.com>
2018-04-20 17:54:35 +0800

    simple login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  if(req.cookies.authorized) {
+       res.render('admin/home', {title: '首页', content: '带cookie登陆成功！'});
+   } else {
+       res.redirect('/login');
+   }
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '登录', flag: 0 });
+});
+
+router.post('/login', function(req, res) {
+  if (req.body.username == 'hello' && req.body.password == 'world') {
+    res.cookie('authorized', req.body.username);
+    res.redirect('/admin');
+  } else{
+        res.render('login', {title: '登录', flag: 1});
+  }
+});
+
 module.exports = router;
